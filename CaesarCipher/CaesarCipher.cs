eb4f01e92a8ce14608e2e97de9feed61abd59a5d
Nataniel Farzan <natanielfarzan@gmail.com>
2025-01-26 21:12:45 -0800

    Initial commit

---
+++ CaesarCipher/CaesarCipher.cs
@@ -0,0 +1,19 @@
+namespace CaesarCipher;
+
+public class CaesarCipher
+{
+    public static string Encode(string message, int shift)
+    {
+        return "";
+    }
+
+    public static string Decode(string message, int shift)
+    {
+        return "";
+    }
+
+    public static string Crack(string encodedMessage)
+    {
+        return "";
+    }
+}
