da2ff9192d6541191e47ac30e0a1f880278aa02b
Nataniel Farzan <natanielfarzan@gmail.com>
2025-01-26 21:41:55 -0800

    Add single letter test

--- CaesarCipher.Tests/UnitTest1.cs
+++ CaesarCipher.Tests/UnitTest1.cs
@@ -2,8 +2,16 @@ namespace CaesarCipher.Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData("a", 1, "b")]
+    [InlineData("b", 1, "c")]
+    [InlineData("z", 1, "a")]
+    [InlineData("a", 25, "z")]
+    [InlineData("d", 25, "c")]
+    public void EncodeSingleLetter(string letter, int shift, string encoded)
     {
+        string encodedLetter = CaesarCipher.Encode(letter, shift);
+        
+        Assert.Equal(encoded, encodedLetter);
     }
 }
