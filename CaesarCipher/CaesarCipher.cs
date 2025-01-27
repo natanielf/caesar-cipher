fc8cf83fee8b87d004e0ec6143aa5db662439799
Nataniel Farzan <natanielfarzan@gmail.com>
2025-01-26 22:34:44 -0800

    Extract common logic to 'ConvertChar' function

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -12,39 +12,55 @@ public class CaesarCipher
         string result = "";
         foreach (char c in message)
         {
-            if (char.IsAsciiLetter(c))
-            {
-                char newChar = (char)(c + shift);
-                if (char.IsAsciiLetterUpper(c))
-                {
-                    if (!char.IsAsciiLetterUpper(newChar))
-                    {
-                        newChar = (char)(newChar % (_uppercaseEnd + 1) + _uppercaseStart);
-                    }
-                } else if (char.IsAsciiLetterLower(c))
-                {
-                    if (!char.IsAsciiLetterLower(newChar))
-                    {
-                        newChar = (char)(newChar % (_lowercaseEnd + 1) + _lowercaseStart);
-                    }
-                }
-                result += newChar;
-            }
-            else
-            {
-                result += c;
-            }
+            char newChar = ConvertChar(c, shift, true);
+            result += newChar;
         }
         return result;
     }
 
     public static string Decode(string message, int shift)
     {
-        return "";
+        shift %= 26;
+        string result = "";
+        foreach (char c in message)
+        {
+            char newChar = ConvertChar(c, shift, false);
+            result += newChar;
+        }
+        return result;
     }
 
     public static string Crack(string encodedMessage)
     {
         return "";
+    }
+
+    private static char ConvertChar(char c, int shift, bool encode)
+    {
+        if (char.IsAsciiLetter(c))
+        {
+            if (!encode)
+            {
+                shift *= -1;
+            }
+
+            char newChar = (char)(c + shift);
+            if (char.IsAsciiLetterUpper(c))
+            {
+                if (!char.IsAsciiLetterUpper(newChar))
+                {
+                    newChar = (char)(newChar % (_uppercaseEnd + 1) + _uppercaseStart);
+                }
+            }
+            else if (char.IsAsciiLetterLower(c))
+            {
+                if (!char.IsAsciiLetterLower(newChar))
+                {
+                    newChar = (char)(newChar % (_lowercaseEnd + 1) + _lowercaseStart);
+                }
+            }
+            return newChar;
+        }
+        return c;
     }
 }
