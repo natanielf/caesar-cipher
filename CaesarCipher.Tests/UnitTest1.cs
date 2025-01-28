1545dddb0f070324b973d488b01f59d013a89d9c
Halle Broadnax <broadnaxhalle@gmail.com>
2025-01-27 19:12:17 -0800

    Create Tests and update Encode & Decode Functions

--- CaesarCipher.Tests/UnitTest1.cs
+++ CaesarCipher.Tests/UnitTest1.cs
@@ -1,9 +1,119 @@
+using CaesarCipher;
 namespace CaesarCipher.Tests;
+
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void ReturnsCorrectEncodeForSingleMessageInput()
     {
+        //Arrange
+        string message = "sandie";
+        int shift = 4;
+        
+        //Act
+        string encoded = CaesarCipher.Encode(message, shift);
+        
+        //Assert
+        Assert.Equal("werhmi", encoded);
+    }
+    
+    //TODO: Test Multiple Word Input
+    [Fact]
+    public void ReturnsCorrectEncodeForMultipleWordMessageInput()
+    {
+        
+    }
+    
+    //TODO: Test Upper and Lower Case
+    [Fact]
+    public void ReturnsCorrectEncodeForUpperAndLowerCaseMessageInput()
+    {
+        //Arrange
+        string message = "Sandie";
+        int shift = 4;
+        
+        //Act
+        string encoded = CaesarCipher.Encode(message, shift);
+        
+        //Assert
+    }
+    //TODO: Test Leading and Trailing Spaces
+    [Fact]
+    public void ReturnsCorrectEncodeForMessageWithLeadingAndTrailingSpaces()
+    {
+        //Arrange
+        string message = " sandie ";
+        int shift = 4;
+        
+        //Act
+        string encoded = CaesarCipher.Encode(message, shift);
+        
+        //Assert
+        
+    }
+    
+    //TODO: Test Non-Alpha Characters
+    [Fact]
+    public void ReturnsCorrectEncodeForMessageWithNonAlphaCharacters()
+    {
+        
+    }
+    //TODO: Test Null Input
+    [Fact]
+    public void ReturnsEmptyEncodeForNullMessageInput()
+    {
+        //Arrange
+        string message = null;
+        int shift = 4;
+        
+        //Act
+        string encoded = CaesarCipher.Encode(message, shift);
+        
+        //Assert
+        Assert.Equal("", encoded);
+    }
+    
+    //TODO: Test Shift of 0
+    [Fact]
+    public void ReturnsCorrectEncodeForZeroShiftInput()
+    {
+        //Arrange
+        string message = "sandie";
+        int shift = 0;
+        
+        //Act
+        string encoded = CaesarCipher.Encode(message, shift);
+        
+        //Assert
+        Assert.Equal("sandie", encoded);
+    }
+    
+    [Fact]
+    public void ReturnsCorrectEncodeForNegativeShiftInput()
+    {
+        //Arrange
+        string message = "sandie";
+        int shift = -22;
+        
+        //Act
+        string encoded = CaesarCipher.Encode(message, shift);
+        
+        //Assert
+        Assert.Equal("werhmi", encoded);
+    }
+    
+    [Fact]
+    public void ReturnsCorrectEncodeForBigShiftInput()
+    {
+        //Arrange
+        string message = "sandie";
+        int shift = 134;
+        
+        //Act
+        string encoded = CaesarCipher.Encode(message, shift);
+        
+        //Assert
+        Assert.Equal("werhmi", encoded);
     }
 }
--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -2,23 +2,22 @@ namespace CaesarCipher;
 
 public static class CaesarCipher
 {
-    private static char[] _alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    
+    private static char[] _alphabet =
+    [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+        'w', 'x', 'y', 'z'
+    ];
+
     public static string Encode(string message, int shift)
     {
-        
         string encodedMessage = string.Empty;
-        message = message.ToLower();
-        
+        message = ValidateMessage(message);
         for (int i = 0; i < message.Length; i++)
         {
             char currentChar = message[i];
             int alphabetIndex = Array.IndexOf(_alphabet, currentChar);
             int encodedIndex = (alphabetIndex + shift) % 26;
-            if (encodedIndex < 0)
-            {
-                encodedIndex += 26;
-            }
+            encodedIndex = ValidateIndex(encodedIndex);
             char encodedChar = _alphabet[encodedIndex];
             encodedMessage += encodedChar;
         }
@@ -29,21 +28,19 @@ public static class CaesarCipher
     public static string Decode(string message, int shift)
     {
         string decodedMessage = string.Empty;
-        message = message.ToLower();
+        message = ValidateMessage(message);
 
         for (int i = 0; i < message.Length; i++)
         {
             char currentChar = message[i];
             int alphabetIndex = Array.IndexOf(_alphabet, currentChar);
             int decodedIndex = (alphabetIndex - shift) % 26;
-            if (decodedIndex < 0)
-            {
-                decodedIndex += 26;
-            }
+            decodedIndex = ValidateIndex(decodedIndex);
             char decodedChar = _alphabet[decodedIndex];
-            
+
             decodedMessage += decodedChar;
         }
+
         return decodedMessage;
     }
 
@@ -55,5 +52,45 @@ public static class CaesarCipher
     private static bool IsCapitalized()
     {
         throw new NotImplementedException();
+    }
+
+    private static string ValidateMessage(string message)
+    {
+        if (message == null)
+        {
+            return "";
+        }
+        else
+        {
+            return message;
+        }
+    }
+
+    private static int ValidateIndex(int index)
+    {
+        if (index < 0)
+        {
+            return index + 26;
+        }
+        else
+        {
+            return index;
+        }
+    }
+
+    private static char ValidateCharacter(char character)
+    {
+        if (character == ' ')
+        {
+            return character;
+        }
+        else if ((!_alphabet.Contains(character)))
+        {
+            return ' ';
+        }
+        else
+        {
+            return character;
+        }
     }
 }
